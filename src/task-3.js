0dd1df82ffa4d83e3a28e8de9410fa4543d5cf8c
Warness <65484013+Warness@users.noreply.github.com>
2020-11-10 23:04:44 +0200

    Иправление работы несколькими методами

--- src/task-3.js
+++ src/task-3.js
@@ -1,37 +1,66 @@
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+// 2 варианта, в 1м выпадала ошибка "Uncaught (in promise) undefined" отловить в rejecte не получилось
 
+// Кривой, но рабочий
+// const makeTransaction = transaction => {
+//     const delay = randomIntegerFromInterval(200, 500);
+//     const canProcess = Math.random() > 0.3;
+
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (canProcess) {
+//         resolve(logSuccess(transaction.id, delay));
+//       } else {
+//         reject(logError(transaction.id))
+//       }
+//     }, delay);
+//   });
+// };
+
+//  Рабочий
 const makeTransaction = transaction => {
-  return new Promise((resolve, reject) => {
-    const delay = randomIntegerFromInterval(200, 500);
-    const canProcess = Math.random() > 0.3;
+  transaction.delay = randomIntegerFromInterval(200, 500);
+  const canProcess = Math.random() > 0.3;
 
-    setTimeout(() => {
-      if (canProcess) {
-        logSuccess(transaction.id, delay)
-        return
-      } else {
-        logError(transaction.id);
-      }
-    }, delay);
-  });
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (canProcess) {
+      resolve(transaction);
+    } else {
+      reject(transaction)
+    }
+  }, transaction.delay);
+});
 };
 
-const logSuccess = (id, time) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
+function logSuccess({id, delay}){
+    console.log(`Transaction ${id} processed in ${delay}ms`);
   };
 
-const logError = id => {
+function logError({id}){
     console.warn(
-      `Error processing transaction ${id}. Please try again later.`,
+      `Error processing transaction ${id}. Please try again later.`
     );
   };
 
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+// Кривой, но рабочий
+// makeTransaction({ id: 70, amount: 150 }).catch(e => {})
 
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 71, amount: 230 }).catch(e => {})
 
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 72, amount: 75 }).catch(e => {})
+
+// makeTransaction({ id: 73, amount: 100 }).catch(e => {})
+
+
+// Рабочий
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess, logError)
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess, logError)
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess, logError)
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess, logError)
